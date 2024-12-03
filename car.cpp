b2ea3b96efdfc4c1f8d756a33e4596b8a8778b08
prefessor joseph <cabdiqaniyacquub8@gmail.com>
2024-12-04 00:11:30 +0300

    this is DEMOJAVA

---
+++ car.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int VEHECLE;
+    cout<<"enter VEHECLE you need\n";
+    cin>>VEHECLE;
+    if(VEHECLE==1){
+        cout<<"car\n";
+    }
+    else if(VEHECLE==2){
+        cout<<"bike\n";
+    }
+    else if(VEHECLE==3){
+        cout<<"van\n";
+    }
+    else if(VEHECLE==4){
+        cout<<"truck\n";
+    }
+    else if(VEHECLE==5){
+        cout<<"motorcycle\n";
+    }
+        else if(VEHECLE==6){
+            cout<<"helicopter\n";
+
+        }
+        else if(VEHECLE==7){
+            cout<<"train\n";
+        }
+        else if(VEHECLE==8){
+            cout<<"airplane\n";
+        }
+         else if(VEHECLE==9){
+                cout<<"boat\n";
+            }
+        
+    
+    else if(VEHECLE==10){
+        cout<<"bus\n";
+    }
+    else 
+    cout<<"we dont have\n";
+}
